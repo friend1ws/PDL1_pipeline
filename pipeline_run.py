d6aaf5145460cf4173cd548142aef9c710c30ab6
friend1ws <friend1ws@gmail.com>
2016-01-03 16:06:44 +0900

    completion until link fastq

--- pipeline_run.py
+++ pipeline_run.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
-import sys, os, argparse, ruffus
+import sys, os, argparse
+from ruffus import *
+
+from read_input import *
 
 ##########
 # store arguments
@@ -21,13 +24,31 @@ args = parser.parse_args()
 
 
 # prepare output directories
-if not os.path.isdir(run_conf.project_root): os.makedirs(run_conf.project_root)
-if not os.path.isdir(run_conf.project_root + '/script'): os.mkdir(run_conf.project_root + '/script')
-if not os.path.isdir(run_conf.project_root + '/log'): os.mkdir(run_conf.project_root + '/log')
-if not os.path.isdir(run_conf.project_root + '/fastq'): os.mkdir(run_conf.project_root + '/fastq')
-if not os.path.isdir(run_conf.project_root + '/virus_count'): os.mkdir(run_conf.project_root + '/virus_count')
-if not os.path.isdir(run_conf.project_root + '/star_ref'): os.mkdir(run_conf.project_root + '/star_ref')
-if not os.path.isdir(run_conf.project_root + '/star'): os.mkdir(run_conf.project_root + '/star')
-if not os.path.isdir(run_conf.project_root + '/fusion'): os.mkdir(run_conf.project_root + '/fusion')
+if not os.path.isdir(args.output_dir): os.makedirs(args.output_dir)
+if not os.path.isdir(args.output_dir + '/script'): os.mkdir(args.output_dir + '/script')
+if not os.path.isdir(args.output_dir + '/log'): os.mkdir(args.output_dir + '/log')
+if not os.path.isdir(args.output_dir + '/fastq'): os.mkdir(args.output_dir + '/fastq')
+if not os.path.isdir(args.output_dir + '/virus_count'): os.mkdir(args.output_dir + '/virus_count')
+if not os.path.isdir(args.output_dir + '/star_ref'): os.mkdir(args.output_dir + '/star_ref')
+if not os.path.isdir(args.output_dir + '/star'): os.mkdir(args.output_dir + '/star')
+if not os.path.isdir(args.output_dir + '/fusion'): os.mkdir(args.output_dir + '/fusion')
 
+
+sample_list_fastq = read_input(args.input_fastq_list)
+
+# generate list of linked_fastq file path
+linked_fastq_list = []
+for sample in sample_list_fastq:
+    linked_fastq_list.append([args.output_dir + '/fastq/' + sample + '/1_1.fastq',
+                              args.output_dir + '/fastq/' + sample + '/1_2.fastq'])
+
+# link the input fastq files
+@originate(linked_fastq_list, sample_list_fastq)
+def link_input_fastq(output_file, sample_list_fastq):
+    sample = os.path.basename(os.path.dirname(output_file[0]))
+    link_dir = args.output_dir + '/fastq/' + sample
+
+    if not os.path.isdir(link_dir): os.mkdir(link_dir)
+    if not os.path.exists(link_dir + '/1.fastq'): os.symlink(sample_list_fastq[sample][0][0], link_dir + '/1.fastq')
+    if not os.path.exists(link_dir + '/2.fastq'): os.symlink(sample_list_fastq[sample][1][0], link_dir + '/2.fastq')
 
